4b7ee3873e7cdf7e32a2f2c497c767107850626e
saddydead1 <luckybeyhuk@gmail.com>
2024-08-04 00:40:30 +0300

    init

---
+++ src/main/kotlin/su/sonoma/subcraftica/Config.kt
@@ -0,0 +1,58 @@
+package su.sonoma.subcraftica
+
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Item
+import net.minecraftforge.common.ForgeConfigSpec
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber
+import net.minecraftforge.fml.event.config.ModConfigEvent
+import net.minecraftforge.registries.ForgeRegistries
+import java.util.stream.Collectors
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Forge's config APIs
+@EventBusSubscriber(modid = Subcraftica.MODID, bus = EventBusSubscriber.Bus.MOD)
+object Config {
+    private val BUILDER = ForgeConfigSpec.Builder()
+
+    private val LOG_DIRT_BLOCK: ForgeConfigSpec.BooleanValue = BUILDER
+        .comment("Whether to log the dirt block on common setup")
+        .define("logDirtBlock", true)
+
+    private val MAGIC_NUMBER: ForgeConfigSpec.IntValue = BUILDER
+        .comment("A magic number")
+        .defineInRange("magicNumber", 42, 0, Int.MAX_VALUE)
+
+    val MAGIC_NUMBER_INTRODUCTION: ForgeConfigSpec.ConfigValue<String> = BUILDER
+        .comment("What you want the introduction message to be for the magic number")
+        .define("magicNumberIntroduction", "The magic number is... ")
+
+    // a list of strings that are treated as resource locations for items
+    private val ITEM_STRINGS: ForgeConfigSpec.ConfigValue<List<String>> = BUILDER
+        .comment("A list of items to log on common setup.")
+        .defineListAllowEmpty("items", listOf("minecraft:iron_ingot")) { obj: Any -> validateItemName(obj) }
+
+    @JvmField
+    val SPEC: ForgeConfigSpec = BUILDER.build()
+
+    var logDirtBlock: Boolean = false
+    var magicNumber: Int = 0
+    var magicNumberIntroduction: String? = null
+    lateinit var items: Set<Item>
+
+    private fun validateItemName(obj: Any): Boolean {
+        return obj is String && ForgeRegistries.ITEMS.containsKey(ResourceLocation(obj))
+    }
+
+    @SubscribeEvent
+    fun onLoad(event: ModConfigEvent?) {
+        logDirtBlock = LOG_DIRT_BLOCK.get()
+        magicNumber = MAGIC_NUMBER.get()
+        magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get()
+
+        // convert the list of strings into a set of items
+        items = ITEM_STRINGS.get()
+            .mapNotNull { itemName: String -> ForgeRegistries.ITEMS.getValue(ResourceLocation(itemName)) }
+            .toSet()
+    }
+}
